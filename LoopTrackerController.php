ac8d4380c04a3efb3f4536752e5bd50adc439131
jepster <p.majmesku@gmail.com>
2015-06-16 17:43:42 +0200

    Some commenting in the class head for documentation.

--- LoopTrackerController.php
+++ LoopTrackerController.php
@@ -4,7 +4,30 @@ namespace app\controllers;
 
 
 /**
- * Class ProductCacheController
+ * Class LoopTrackerController
+ *
+ * This class is designed to track the status of a loop for you. So if you have
+ * a loop like
+ *
+ * while ($while_loop_counter <= $total_num_property_values) {
+ *      doSomething();
+ * }
+ *
+ * you can integrate the LoopTracker here by modifying the code like so:
+ *
+ * $loop_tracker = new LoopTrackerController();
+ * while ($while_loop_counter <= $total_num_property_values) {
+ *      doSomething();
+ *      $loop_tracker->track("Property Value Fetching from DB", $total_num_property_values);
+ * }
+ *
+ * Afterwards LoopTracker will produce echo's which are useful for the command line. They'll
+ * look like so:
+ *
+ * Processing property values..
+ * 18300 of 642249 / 2% / elapsed seconds: 17 / expected seconds: 547 / expected finish at: 13:34:14
+ *
+ *
  * @package app\controllers
  */
 class LoopTrackerController
